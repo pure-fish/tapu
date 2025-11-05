ba4a112d0326787602ff3683dfe01ffa4b943c05
Édouard Lopez <contact@edouard-lopez.com>
2025-11-05 12:27:58 +0100

    refactor: split tapu into modular function files

--- functions/tapu.fish
+++ functions/tapu.fish
@@ -1,161 +1,16 @@
 #!/usr/bin/env fish
 # TAP (Test Anything Protocol) Reporter for Fish Shell
-# A pure fish implementation similar to tap-diff/tap-mocha-reporter
+# A pure fish implementation similar to tap-diff
 # Formats TAP output with colors, symbols, and timing information
 
-set --local --export INDENT '  '
-set --local --export TICK '✔'
-set --local --export CROSS '✖'
+# Load helper modules
+set --local module_dir (dirname (realpath (status filename)))
+source $module_dir/tapu/colors.fish
+source $module_dir/tapu/formatting.fish
 
-# Color helpers (compatible with fish set_color)
-function _color_green
-    set_color green
-    echo -n $argv
-    set_color normal
-end
-
-function _color_red
-    set_color red
-    echo -n $argv
-    set_color normal
-end
-
-function _color_blue
-    set_color blue
-    echo -n $argv
-    set_color normal
-end
-
-function _color_yellow
-    set_color yellow
-    echo -n $argv
-    set_color normal
-end
-
-function _color_magenta
-    set_color magenta
-    echo -n $argv
-    set_color normal
-end
-
-function _color_dim
-    set_color brblack
-    echo -n $argv
-    set_color normal
-end
-
-function _color_white
-    set_color white
-    echo -n $argv
-    set_color normal
-end
-
-# Unescape TAP escape sequences (\# and \\)
-function _unescape_tap
-    set --local input "$argv[1]"
-    # Replace \\ with a placeholder first to avoid double-unescaping
-    set input (string replace -a '\\\\' '\x00' -- "$input")
-    # Replace \# with #
-    set input (string replace -a '\\#' '#' -- "$input")
-    # Replace placeholder back with single \
-    set input (string replace -a '\x00' '\\' -- "$input")
-    echo "$input"
-end
-
-# Print with indentation
-function _println
-    set --local input "$argv[1]"
-    set --local indent_level 0
-    if set -q argv[2]
-        set indent_level $argv[2]
-    end
-    
-    set --local indent_str ''
-    for i in (seq 1 $indent_level)
-        set indent_str "$indent_str$INDENT"
-    end
-    
-    if test -z "$input"
-        echo
-    else
-        echo "$input" | while read -l line
-            echo "$indent_str$line"
-        end
-    end
-end
-
-# Format milliseconds to human readable
-function _format_ms
-    set --local ms $argv[1]
-    
-    if test $ms -lt 1000
-        echo $ms"ms"
-        return
-    end
-    
-    set --local seconds (math "floor($ms / 1000)")
-    set --local minutes (math "floor($seconds / 60)")
-    set --local hours (math "floor($minutes / 60)")
-    
-    if test $hours -gt 0
-        set minutes (math "$minutes % 60")
-        set seconds (math "$seconds % 60")
-        echo "{$hours}h {$minutes}m {$seconds}s"
-    else if test $minutes -gt 0
-        set seconds (math "$seconds % 60")
-        echo "{$minutes}m {$seconds}s"
-    else
-        echo "{$seconds}s"
-    end
-end
-
-# Highlight differences between two strings
-function _highlight_diff
-    set --local actual "$argv[1]"
-    set --local expected "$argv[2]"
-    
-    # If strings are identical, just return them
-    if test "$actual" = "$expected"
-        echo "Actual/Expected: $actual"
-        return
-    end
-    
-    # Show actual with red background for differences
-    echo -n "Actual:   "
-    set --local actual_len (string length -- "$actual")
-    set --local expected_len (string length -- "$expected")
-    set --local max_len (math "max($actual_len, $expected_len)")
-    
-    for i in (seq 1 $actual_len)
-        set --local actual_char (string sub -s $i -l 1 -- "$actual")
-        set --local expected_char (string sub -s $i -l 1 -- "$expected")
-        
-        if test "$actual_char" != "$expected_char"
-            set_color -b red
-            echo -n "$actual_char"
-            set_color normal
-        else
-            echo -n "$actual_char"
-        end
-    end
-    echo
-    
-    # Show expected with green background for differences
-    echo -n "Expected: "
-    for i in (seq 1 $expected_len)
-        set --local actual_char (string sub -s $i -l 1 -- "$actual")
-        set --local expected_char (string sub -s $i -l 1 -- "$expected")
-        
-        if test "$actual_char" != "$expected_char"
-            set_color -b green
-            echo -n "$expected_char"
-            set_color normal
-        else
-            echo -n "$expected_char"
-        end
-    end
-    echo
-end
+set --export INDENT '  '
+set --export TICK '✔'
+set --export CROSS '✖'
 
 # Main TAP parser
 function tapu
